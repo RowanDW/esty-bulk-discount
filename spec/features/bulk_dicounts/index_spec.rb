08fe82f9084e5b70eda75ed02e7ad571d8814de9
Rowan DeLong <rowanwinzer@gmail.com>
2021-09-24 12:42:42 -0600

    Add test for link to new discount page

--- spec/features/bulk_dicounts/index_spec.rb
+++ spec/features/bulk_dicounts/index_spec.rb
@@ -36,4 +36,13 @@ RSpec.describe "the bulk discounts index page" do
 
     expect(current_path).to eq(merchant_bulk_discount_path(@merch1, @discounts[0]))
   end
+
+  it "has a link to create a new bulk discount" do
+    visit merchant_bulk_discounts_path(@merch1)
+
+    expect(page).to have_button("New Discount")
+    click_button "New Discount"
+
+    expect(current_path).to eq(new_merchant_bulk_discount(@merch1))
+  end
 end
